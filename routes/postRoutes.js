b7bd36e8820b1339b9fec9f75f9133e609f431f0
natannascimento <natan.graves@gmail.com>
2023-08-23 13:21:03 -0300

    Ajustes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,7 @@
-// routes/postRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-// Defina as rotas para criar, atualizar, deletar e buscar posts
-// Utilize middlewares para tratar as requisições
 
 module.exports = router;
